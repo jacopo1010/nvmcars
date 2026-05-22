cf5d54e0ef24a73f1dfe2e2f44d1cf842716ca04
jacopo1010 <jaorru03@gmail.com>
2026-05-23 00:26:45 +0200

    aggiungere mail e iniziare a vedere i permessi e fare le prime prove da frontend

--- src/main/java/org/nvm/cars/utils/EmailService.java
+++ src/main/java/org/nvm/cars/utils/EmailService.java
@@ -1,4 +1,14 @@
 package org.nvm.cars.utils;
 
 public class EmailService {
+
+    public EmailService(){
+
+    }
+
+    public void sendEmail(String email, String subject, String body){
+
+    }
+
+
 }
